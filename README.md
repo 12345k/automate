98bafff74399532c0a6b0605d4e9eff53423c346
unknown <karthickaravindan@gmail.com>
2018-04-13 23:04:43 +0530

    Ckarthic version

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# automate
+# pyRec
+a sample program to show how to automate repetitive mouse and keyboard activities.
 
-##Windows notepad try
+To use this program, you need to install pyHook, PyWin32 and PyAutoGui.
+
+A detailed description can be found <a href="http://ycsu.pythonanywhere.com/project/18">here</a>.
+
+Record mouse clicks:
+```
+python record.py session_name
+```
+Replay:
+```
+python replay.py session_name
+```
--- replay_NLP.py
+++ replay_NLP.py
@@ -1,21 +1,56 @@
-# -*- coding: cp950 -*-
 import time
 import os
 import sys
 import pyautogui
-import ParseHistory as ph
+import nltk
+import numpy
 
+
+# Simple usage
+from stanfordcorenlp import StanfordCoreNLP
+
+stf_path = r'D:\Balaji\sharedfolder\nltk_rec\stanford-corenlp-full-2018-02-27'
+
+if len(sys.argv) > 1:
+    sentence = sys.argv[1]
+    nlp = StanfordCoreNLP(stf_path)
+    # sentence = "Send invoice to Optisol Inc., for amount of $1000 for work done on April 2018"
+    named_entity =  nlp.ner(sentence)
+    # print('Named Entities:', named_entity)
+    nlp.close() # Do not forget to close! The backend server will consume a lot memery.
+
+    temp = {}
+    for i, entity in enumerate(named_entity):
+        if entity[1] is not 'O':
+            if entity[1] in temp:
+                temp[entity[1]] = temp[entity[1]] + ' '+ entity[0]
+            else:
+                temp[entity[1]] = entity[0]
+        else:
+            if entity[0] == 'to' and "NAME" not in temp:
+                temp["NAME"] = named_entity[i+1][0]
+    print(temp)
+    if "DATE" not in temp or "MONEY" not in temp:
+        print("Try something like")
+        print("'Send offerletter to Joe Smith for $500 on April 2018'")
+    else:
+        # print("Great! working out")
+        sys.exit()
+
+else:
+    print("Please enter a sentence to continue!!!")
+    exit()
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 directory = 'mouse_recorder'
 try:
-    session_name = 'SendOfferLetter' # sys.argv[1]
+    session_name = 'helloworld' # sys.argv[1]
 except:
     print ('you must enter a name for the session\nfor example: python replay.py session_name')
     sys.exit()
 dir_path = os.path.join(os.getcwd(), directory, session_name)
 
-file_name = 'OfferLetterScripts.txt'
+file_name = 'history.txt'
 file_path = os.path.join(dir_path, file_name)
 #print dir_path
 
@@ -23,11 +58,6 @@ print(file_path)
 # open the recording file
 with open(file_path, 'r') as f:
     steps = f.readlines()
-
-steps = ph.parse()    
-#for s in steps:
-#    print (s)
-
 
 print('-'.join(['# of steps', str(len(steps))]))
 # clean steps
@@ -40,10 +70,6 @@ for step in steps:
     #print('.'.join(['new_step', new_step]))
     new_steps.append(new_step)
 
-for ns in new_steps:
-    print(ns)
-    
-sys.exit()
 
 # start moving mouse cursor
 t_last = float(new_steps[0][-1])
@@ -54,14 +80,15 @@ keytranslations = {'Oem_Minus' : '_',
                    'Lshift' : 'shift'}
 
 skipcount = 0
-#skip = False
+# skip = False
 for i, step in enumerate(new_steps):
-    print(step[0])
-    if skipcount > 1 and step[0] != 'done':
+    print(step)
+    sys.exit()
+    if skipcount > 1:
         skipcount = skipcount - 1
         t_last = float(step[-1])
         continue
-
+    print(step[0])
     if step[0] == 'mouse left down':
         
         time.sleep(float(step[-1]) - t_last)
@@ -103,13 +130,17 @@ for i, step in enumerate(new_steps):
         pyautogui.hotkey(step[2].lower())
         print(step[2].lower())
         t_last = float(step[-1])
-    
-    if(step[0] == 'type write'):
-        pyautogui.typewrite(step[1], interval=0.05)
         
-    if (step[0] == 'done'):
+    if step[0] == 'done':
         print ('End autorun')
         sys.exit()
-
-sys.exit()
     
+    
+def trysomething():
+    time.sleep(5)
+    print('trying something')
+    #pyautogui.hotkey('ctrl', 'a')
+    pyautogui.keyDown('shift')
+    pyautogui.keyDown('end')
+    pyautogui.keyUp('end')
+    pyautogui.keyUp('shift')
